0903a4b2b5e1a2bf813e45e6c855243f49564eed
ayyyk123 <ayyyk123@gmail.com>
2026-08-19 23:14:48 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='household-book-v45-1-reminder-summary-layout-20260818-1';
+const CACHE='household-book-v45-2-title-area-toggle-20260819-2';
 const APP_ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png','./apple-touch-icon.png'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(APP_ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
