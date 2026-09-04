f49db8cfbc832e79225ecaede93fe726b26956ef
ayyyk123 <ayyyk123@gmail.com>
2026-09-04 17:05:47 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='household-book-v46-5-reimbursement-budget-diff-20260904-1';
+const CACHE='household-book-v46-6-reimbursement-budget-diff-layout-20260904-1';
 const APP_ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png','./apple-touch-icon.png'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(APP_ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
